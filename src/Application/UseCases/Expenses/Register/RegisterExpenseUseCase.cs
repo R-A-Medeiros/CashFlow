151738a046454f707e55ffc3954f9d2502216f4f
R-A-Medeiros <r.medeiros1206@gmail.com>
2024-12-22 16:05:45 -0300

    Implementação AutoMapper

--- src/Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories;
@@ -12,11 +13,13 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
 {
     private readonly IExpensesRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public RegisterExpenseUseCase(IExpensesRepository repository, IUnitOfWork unitOfWork)
+    public RegisterExpenseUseCase(IExpensesRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
     public async Task<ResponseRegisteredExpenseJson> Execute(RequestRegisterExpenseJson request)
@@ -24,20 +27,23 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
         // Validations
         Validate(request);
 
-        var entity = new Expense
-        {
-            Amount = request.Amount,
-            Date = request.Date,
-            Description = request.Description,
-            Title = request.Title,
-            PaymentType = (Domain.Enums.PaymentType)request.PaymentType
+        //var entity = new Expense
+        //{
+        //    Amount = request.Amount,
+        //    Date = request.Date,
+        //    Description = request.Description,
+        //    Title = request.Title,
+        //    PaymentType = (Domain.Enums.PaymentType)request.PaymentType
 
-        };
+        //};
+
+        var entity = _mapper.Map<Expense>(request);
 
         await _repository.Add(entity);
         await _unitOfWork.Commit();
 
-        return new ResponseRegisteredExpenseJson();
+        //   return new ResponseRegisteredExpenseJson();
+        return _mapper.Map<ResponseRegisteredExpenseJson>(entity);
     }
 
     public void Validate(RequestRegisterExpenseJson request)
