89252ae796b0ca4353f8eb0e1408a47279b690f9
R-A-Medeiros <r.medeiros1206@gmail.com>
2024-12-23 22:48:28 -0300

    Endpoint GetAll

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Application.UseCases.Expenses.Register;
+﻿using CashFlow.Application.UseCases.Expenses.GetAll;
+using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
@@ -23,5 +24,19 @@ public class ExpensesController : ControllerBase
 
         return Created(string.Empty, response);
     }
-        
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseExpenseJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetAllExpenses([FromServices] IGetAllExpenseUseCase useCase)
+    {
+        var response = await useCase.Execute();
+
+        if (response.Expenses.Count != 0)
+            return Ok(response);
+
+
+        return NoContent();
+    }
+
 }
