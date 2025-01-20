e0b27e515495c145cfca875811245ce95f9f3925
R-A-Medeiros <r.medeiros1206@gmail.com>
2025-01-19 22:32:36 -0300

    endpoint para pdf

---
+++ src/Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -0,0 +1,22 @@
+﻿using CashFlow.Domain.Repositories.Expenses;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
+{
+    private readonly IExpensesRepository _repository;
+    public GenerateExpensesReportPdfUseCase(IExpensesRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var expense = await _repository.FilterByMonth(month);
+        if(expense.Count == 0)
+        {
+            return [];
+        }
+
+        return [];
+    }
+}
