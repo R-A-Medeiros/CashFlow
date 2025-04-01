1812bccc46a64ba1ff4632b6d4503bb33e80f91a
Rinaldo Alves <r.medeiros1206@gmail.com>
2025-03-31 22:16:54 -0300

    implementando login

---
+++ src/CashFlow.Exception/ExceptionBase/InvalidLoginException.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Net;
+
+namespace CashFlow.Exception.ExceptionBase;
+
+public class InvalidLoginException : CashFlowException
+{
+    public InvalidLoginException() : base("Email or Password invalid.")
+    {
+
+    }
+
+    public override int StatusCode => (int)HttpStatusCode.Unauthorized;
+
+    public override List<string> GetErrors()
+    {
+        return [Message];
+    }
+}
