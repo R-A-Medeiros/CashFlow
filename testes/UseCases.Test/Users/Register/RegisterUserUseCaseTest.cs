d85116690ecc2415f1aa1ca51dfce15c4d4dd589
Rinaldo Alves <r.medeiros1206@gmail.com>
2025-04-07 22:18:41 -0300

    mocks

---
+++ testes/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Application.UseCases.User;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using Shouldly;
+
+namespace UseCases.Test.Users.Register;
+
+public class RegisterUserUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var useCase = CreateUserCase();
+
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(request.Name);
+        result.Token.ShouldNotBeNullOrEmpty();
+    }
+
+    private RegisterUserUseCase CreateUserCase()
+    {
+        var mapper = MapperBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
+
+        return new RegisterUserUseCase(mapper, null, null, writeRepository, unitOfWork, null);
+    }
+}
